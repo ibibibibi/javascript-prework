085d902646455849ab8d98a636696e05cd06fcca
bibi <baranowskabeata1@gmail.com>
2020-03-01 13:55:28 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, userMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+userMove = 'papier';
+printMessage('Zagrałem ' + userMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
