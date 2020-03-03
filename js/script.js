902d5331832389d59dfb04840851633c2eb05061
bibi <baranowskabeata1@gmail.com>
2020-03-03 18:37:22 +0100

    edit script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var computerMove, userMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-userMove = 'papier';
-printMessage('Zagrałem ' + userMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber =='1') {
+    computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove == 'papier' ;
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce' ;
+} else {
+    computerMove = 'nieznany ruch' ;
+}
+printMessage ('Mój ruch: ' + computerMove)
